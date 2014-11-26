1612e0655f5f92fdddb6c43025ab6e132fa0d6b8
lianxx <lianxx_os@sari.ac.cn>
2014-11-26 16:34:20 +0800

    git commit test

--- Source/BrowserApp/settings/BrowserSettingsDialog.c
+++ Source/BrowserApp/settings/BrowserSettingsDialog.c
@@ -419,6 +419,34 @@ static void initMediaAccessRadioButton(WebKitSettings *settings)
 
 }
 
+//lxx add +, 14.11.17
+static void initAskEverytimeBeforeDownloadfile(WebKitSettings *settings)
+{
+	bool bvalue;
+	g_object_get(settings,
+					key[PROP_ASK_EVERYTIME_BEFORE_DOWN], &bvalue,
+					NULL);
+	if(TRUE == bvalue) 
+	 {
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(settings->checkbutton1_advanced), TRUE);     
+	} 
+	else 
+	{
+		gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(settings->checkbutton1_advanced), FALSE); 
+	}
+}
+
+static void initStorePathOfDownloadPathEntry(WebKitSettings *settings)
+{
+    gchar *strval;
+    g_object_get(settings,
+             key[PROP_STORE_PATH_OF_DOWNFILE], &strval,
+             NULL);
+    gtk_entry_set_text(GTK_ENTRY(settings->entry1_advanced), strval);
+    g_free (strval);
+}
+//lxx add -, 14.11.17
+
 /*Get new state, init corresponding widgets. sunhaiming add.*/
 static void browserSettingsWindowConstructed(WebKitSettings *settings)
 {
@@ -448,6 +476,8 @@ static void browserSettingsWindowConstructed(WebKitSettings *settings)
     initTrackLocationRadioButton(settings);
     initCertificateRevocationCheckButton(settings);
     initMediaAccessRadioButton(settings);  
+	initAskEverytimeBeforeDownloadfile(settings);
+    initStorePathOfDownloadPathEntry(settings);
 }
 
 //clear data callback 
@@ -790,7 +820,191 @@ static void mediaAccessCallback(GtkToggleButton *togglebutton, WebKitSettings *s
                key[PROP_MEDIA_ACCESS], ivalue,
                NULL);
     }
-} 
+}
+//lxx add +, 14.11.13 
+static void setCuprumDefaultBrowserCallback(GtkButton *button, WebKitSettings *settings)
+{
+	system("xdg-settings set default-web-browser cuprumbrowser.desktop");
+	printf("function setCuprumDefaultBrowserCallback has been called\n");
+}
+
+static void networkSettingCallback(GtkButton *button, WebKitSettings *settings)
+{
+	system("/usr/bin/cinnamon-settings network");
+	printf("function networkSettingCallback has been called\n");
+}
+
+static void do_reset_browser()
+{
+	if(DeletePreferencesFile())
+	{
+		printf("PreferencesFile has been deleted\n");
+		return;
+	};
+//删除历史记录的
+//删除书签的
+	printf("do reset browser func has been called\n");
+}
+
+static void resetNetworkSettingCallback(GtkButton *button, WebKitSettings *settings)
+{
+#if 0//使用创建一个主窗口的方式来解决
+	GtkWidget *window;
+	GtkWidget *table;
+
+	GtkWidget *Hbox;  
+	GtkWidget *button_OK;
+	GtkWidget *button_cancel;
+	GtkWidget *Vbox;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+	gtk_window_set_title(GTK_WINDOW(window), "确实要重置浏览器吗？");
+
+	GtkWidget *info_label;
+
+	info_label = gtk_label_new("该操作将重置您的主页、将您的常规设置、\n隐私和安全等设置恢复为原始默认设置，还\n将删除您的书签、历史记录以及临时数据和\n缓存数据（例如Cookie）。");
+
+	Vbox = gtk_vbox_new(TRUE,0);  
+	Hbox = gtk_hbox_new(TRUE,0);  
+	button_OK = gtk_button_new_with_label("重    置");  
+	button_cancel = gtk_button_new_with_label("取    消");
+
+    //put OK and cancel box into HBox  
+	gtk_box_pack_start(GTK_BOX(Hbox),button_OK,FALSE,FALSE,0);  
+	gtk_box_pack_start(GTK_BOX(Hbox),button_cancel,FALSE,FALSE,0);  
+
+	gtk_box_pack_start(GTK_BOX(Vbox),info_label,FALSE,FALSE,0);  
+	gtk_box_pack_start(GTK_BOX(Vbox),GTK_WIDGET(Hbox),FALSE,FALSE,0);  
+
+	gtk_container_add(GTK_CONTAINER(window),Vbox);  
+
+//set this window unresizable,but this function doesn't work here
+	//gtk_window_set_resizable( GTK_WINDOW(window), 0);
+	//gtk_widget_show(fixedp);
+
+
+	g_signal_connect(G_OBJECT(button_OK), "clicked", G_CALLBACK(do_reset_browser), (gpointer) window);
+
+	g_signal_connect(G_OBJECT(button_cancel), "clicked", G_CALLBACK(cancel_reset_browser), (gpointer) window);
+
+//destroy this window
+	//g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), G_OBJECT(window));
+
+	gtk_widget_show_all(window);
+
+//gtk_window_set_policy(GTK_WINDOW(window), FALSE, FALSE, TRUE);
+//gtk_window_set_resizable( GTK_WINDOW(window), 0);
+#endif
+
+	GtkWidget *dialog;
+	dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_QUESTION, GTK_BUTTONS_OK_CANCEL,
+                                                                    "该操作将重置您的主页、将您的常规设置、\n隐私和安全等设置恢复为原始默认设置，还\n将删除您的书签、历史记录以及临时数据和\n缓存数据（例如Cookie）。");
+	gtk_window_set_title (GTK_WINDOW (dialog), "确实要重置浏览器吗？");
+	gint result = gtk_dialog_run(GTK_DIALOG (dialog));
+	if(result == GTK_RESPONSE_CANCEL)
+	{
+		gtk_widget_destroy(dialog);
+	}
+	else if(result == GTK_RESPONSE_OK)
+	{
+		do_reset_browser();
+		gtk_widget_destroy(dialog);
+	}
+	gtk_widget_destroy (dialog);
+
+	printf("function resetNetworkSettingCallback has been called\n");
+
+}
+
+static void alterDownloadSaveCatalogCallback(GtkButton *button, WebKitSettings *settings)
+{
+#if 0
+GtkWidget* widget;
+
+        widget = gtk_file_chooser_button_new ("Choose folder",
+            GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
+
+        gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER (widget),
+                                             "/etc");
+	gtk_widget_show_all(widget);
+   //     if (pspec->flags & G_PARAM_WRITABLE)
+   //         g_signal_connect (widget, "selection-changed",
+   //                           G_CALLBACK (proxy_folder_file_set_cb), object);
+
+printf("function alterDownloadSaveCatalogCallback has been called\n");
+#else
+	GtkWidget *dialog;
+	GtkWidget *entry;
+
+    ///创建文件选择对话框
+	dialog = gtk_file_chooser_dialog_new ("保存目录",
+							NULL,/*gtk_widget_get_toplevel(GTK_WIDGET(button)),*/
+							GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,//GTK_FILE_CHOOSER_ACTION_SAVE, 
+							GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+							GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
+							NULL);
+	entry = (GtkEntry*)settings->entry1_advanced;
+
+    ///文件选择类型过滤
+
+/*    //所有文件
+    GtkFileFilter* filter = gtk_file_filter_new();
+    gtk_file_filter_set_name (filter, "All files");
+    gtk_file_filter_add_pattern(filter,"*.");
+    gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (dialog),filter);
+
+    //后缀为.txt的文件
+    filter = gtk_file_filter_new();
+    gtk_file_filter_set_name (filter, "Txt text files");
+    gtk_file_filter_add_pattern(filter,"*.[Tt][Xx][Tt]");
+    gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (dialog),filter);
+
+    ///文件类文rm/rmvb的文件
+    filter = gtk_file_filter_new();
+    gtk_file_filter_set_name (filter, "rm/rmvb files");
+//    gtk_file_filter_add_pattern(filter,"*.[rR][mM]");
+//    gtk_file_filter_add_pattern(filter,"*.[rR][mM][vv][bB]");
+    gtk_file_filter_add_mime_type (filter,"application/vnd.rn-realmedia");
+    gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (dialog),filter);
+
+    gtk_file_chooser_set_local_only (GTK_FILE_CHOOSER (dialog), FALSE);
+*/
+    //设置当前文件夹
+	gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER (dialog),"/etc");
+
+	gtk_window_set_position(GTK_WINDOW(dialog),GTK_WIN_POS_CENTER_ON_PARENT);
+
+    ///等待用户的动作
+	if(gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
+	{
+        ///取得用户所选文件的路径
+		char *filename;
+		filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
+
+        ///把文件路径显示到输入条上
+		gtk_entry_set_text(entry,filename);
+
+//将这里的设置写到设置文件里去
+		g_object_set(settings,
+					 key[PROP_STORE_PATH_OF_DOWNFILE], filename,
+					 NULL);
+		g_free(filename);
+	}
+	gtk_widget_destroy(dialog);
+#endif
+
+}
+
+static void askEverytimeBeforeDownloadCallback(GtkToggleButton *button, WebKitSettings *settings)
+{
+    bool bvalue = gtk_toggle_button_get_active(button); 
+    g_object_set(settings,
+             key[PROP_ASK_EVERYTIME_BEFORE_DOWN], bvalue,
+             NULL);
+}
+//lxx add -, 14.11.13 
 
 /*Build settings window from the glade file. Connecting widgets we interest. sunhaiming add.*/
 GtkWidget * browser_settings_window_new(WebKitSettings *settings)
@@ -925,6 +1139,24 @@ GtkWidget * browser_settings_window_new(WebKitSettings *settings)
 
    settings->radiobutton2_security = GTK_WIDGET(gtk_builder_get_object(builder, "radiobutton2_security"));
    g_signal_connect(G_OBJECT(settings->radiobutton2_security), "toggled", G_CALLBACK(mediaAccessCallback), settings);
+
+//lianxx add +, 14.11.13，advanced setting
+   settings->button1_advanced = GTK_WIDGET(gtk_builder_get_object(builder, "button1_advanced"));
+   g_signal_connect(G_OBJECT(settings->button1_advanced), "clicked", G_CALLBACK(setCuprumDefaultBrowserCallback), settings);
+
+   settings->button2_advanced = GTK_WIDGET(gtk_builder_get_object(builder, "button2_advanced"));
+   g_signal_connect(G_OBJECT(settings->button2_advanced), "clicked", G_CALLBACK(networkSettingCallback), settings);
+
+   settings->button3_advanced = GTK_WIDGET(gtk_builder_get_object(builder, "button3_advanced"));
+   g_signal_connect(G_OBJECT(settings->button3_advanced), "clicked", G_CALLBACK(resetNetworkSettingCallback), settings);
+
+   settings->button4_advanced = GTK_WIDGET(gtk_builder_get_object(builder, "button4_advanced"));
+   g_signal_connect(G_OBJECT(settings->button4_advanced), "clicked", G_CALLBACK(alterDownloadSaveCatalogCallback), settings);
+
+   settings->checkbutton1_advanced = GTK_WIDGET(gtk_builder_get_object(builder, "checkbutton1_advanced"));
+   g_signal_connect(G_OBJECT(settings->checkbutton1_advanced), "toggled", G_CALLBACK(askEverytimeBeforeDownloadCallback), settings);
+
+   settings->entry1_advanced = GTK_WIDGET(gtk_builder_get_object(builder, "entry1_advanced"));
 
    browserSettingsWindowConstructed(settings);
    g_object_unref (G_OBJECT (builder));
