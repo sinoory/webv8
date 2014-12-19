51d250d5166e987e1830c0158c2b0c6f6b1558be
wangjy <wangjy_os@sari.ac.cn>
2014-12-19 15:38:29 +0800

    remove unnecessary cmake generated files

--- Source/BrowserApp/web-extension/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangjiayu/workspace/newclean/Webkit2Browser/Source/BrowserApp/web-extension
-# Build directory: /home/wangjiayu/workspace/newclean/Webkit2Browser/Source/BrowserApp/web-extension
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
