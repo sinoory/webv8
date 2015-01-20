ac82e57cc3f37e68464eeb357fbd3f3c2b4f99dc
lianxx <lianxx_os@sari.ac.cn>
2015-01-20 15:15:32 +0800

    lxx update file

--- Source/WebKit2/UIProcess/API/gtk/WebKitWebView.h
+++ Source/WebKit2/UIProcess/API/gtk/WebKitWebView.h
@@ -256,6 +256,9 @@ struct _WebKitWebViewClass {
     gboolean   (* console_message)              (WebKitWebView               *web_view,
                                                 const gchar                 *message,
                                                 unsigned lineNumber, const char* sourceID);
+    // lxx define dnt_http_header, 20150116
+    gboolean   (* dnt_http_header)              (WebKitWebView               *web_view,
+                                                WebKitURIRequest *requestRequest);
     void (*_webkit_reserved0) (void);
     void (*_webkit_reserved1) (void);
     void (*_webkit_reserved2) (void);
