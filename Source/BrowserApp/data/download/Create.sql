4cebd5120d38d85e8e46489205abeed4f49e8a90
zgh <zhanggh_os@sari.ac.cn>
2015-02-28 11:23:44 +0800

    zgh 下载database创建脚本提交

---
+++ Source/BrowserApp/data/download/Create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS download
+(
+    name text,
+    destination text,
+    file_size text,
+    uri text,
+    create_time integer,
+    content_type text,
+    download_progress integer
+);
+
